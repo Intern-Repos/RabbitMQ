a943e3f5f82f9f21b5d7e857cb94993727f686e6
kobinarth-panchalingam <kobinarth22@gmail.com>
2023-12-06 15:44:53 +0530

    finished the json conver message queue

--- src/main/java/com/gtn/rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/gtn/rabbitmq/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package com.gtn.rabbitmq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +18,22 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
 
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
+    @Value("${rabbitmq.routing.json.key}")
+    private String jsonRoutingKey;
+
     //spring bean for rabbitmq queue
     @Bean
     public Queue queue() {
         return new Queue(queue);
     }
 
+    //spring bean for rabbitmq json queue
+    @Bean
+    public Queue jsonQueue() {
+        return new Queue(jsonQueue);
+    }
     //spring bean for rabbitmq exchange
     @Bean
     public TopicExchange exchange() {
@@ -36,4 +47,25 @@ public class RabbitMQConfig {
                 .to(exchange())
                 .with(routingKey);
     }
+
+    //spring bean for binding between json queue and exchange
+    @Bean
+    public Binding jsonBinding(){
+        return BindingBuilder.bind(jsonQueue())
+                .to(exchange())
+                .with(jsonRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+
 }
